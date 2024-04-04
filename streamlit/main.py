1c6184e1f4763a4f0fd7e1f1a41b938d848ab02d
Bruna Los <brunalos17@gmail.com>
2024-04-04 18:59:31 +0200

    style: added comments

--- streamlit/main.py
+++ streamlit/main.py
@@ -3,7 +3,16 @@ import pickle
 import pandas as pd
 
 
-st.title("Dropout Rates Predictor")
+# Create columns
+col1, col2 = st.columns([3, 1])
+# Add title to the left side
+with col1:
+    st.title("Dropout Rates Predictor")
+# Add image to the right side
+with col2:
+    st.image("https://github.com/brunalos/brunalos/assets/27739711/ec178d09-2dbb-43f2-83e5-96b9065cdd4b", width=60)
+
+
 st.write("\n\n")
 
 st.write("#### Predicting Participant's Dropout Rates in Vaccine Clinical Trials")
@@ -22,14 +31,27 @@ phase_options = {
     'Not Applicable': 0
 }
 
+# Input Data Section: Add interactive elements for user input
 st.write("##### Input Data")
 col1, col2 = st.columns(2)
-enrollment = col1.number_input("Enrollment", min_value=0)
+
+# Enrollment: Numeric input for the number of participants enrolled in the trial
+enrollment = col1.number_input("Enter Enrollment Count", min_value=0)
+
+# AE Total: Numeric input for the total number of Serious Adverse Events reported
 AE_total = col1.number_input(
-    "Total Number of Serious Adverse Events", min_value=0)
+    "Enter Total Number of Serious Adverse Events", min_value=0)
+
+# Phase: Dropdown selection box to choose the phase of the clinical trial
 phase = col1.selectbox("Select Phase", list(phase_options.keys()))
-duration = col2.number_input("Duration of the Trial (in months)", min_value=0)
-mean_age = col2.number_input("Mean Age of the Cohort", min_value=0)
+
+# Duration: Numeric input for the duration of the trial in months
+duration = col2.number_input(
+    "Enter Duration of the Trial (in months)", min_value=0)
+
+# Mean Age: Numeric input for the mean age of the participants
+mean_age = col2.number_input(
+    "Enter Mean Age of the Participants", min_value=0)
 
 
 # Load the model
@@ -65,7 +87,7 @@ if st.button('Run'):
 
 st.write("\n\n")
 st.write("\n\n")
-st.write("###### Disclaimer")
+st.write("#### Disclaimer")
 st.write("\n\n")
 st.write("The Random Forest model was the most promising predictor for dropout rates in vaccine clinical trials.")
 st.write("\n\n")
